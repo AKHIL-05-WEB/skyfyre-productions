712479d27cbd8c4ff265de5d847338f7eb186f72
AKHIL-05-WEB <akhil0708jnv@gmail.com>
2025-06-01 21:32:05 +0530

    product-helpers.js

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,158 +1,150 @@
-const collection = require('../config/collection');
-const { ObjectId } = require('mongodb');
-var db = require('../config/connection');
+const db = require('../config/connection');
+
 module.exports = {
-    addProduct: (product, callback) => {
-        db.getDB().collection('products').insertOne(product).then((data) => {
-            const id = data.insertedId;
-            const imagePath = `images/product-images/${id}.png`;
+  addProduct: async (product, callback) => {
+    try {
+      const dbInstance = db.getDB();  // get DB *here*, inside function
+      const data = await dbInstance.collection('products').insertOne(product);
+      const id = data.insertedId;
+      const imagePath = `images/product-images/${id}.png`;
 
-            db.getDB().collection('products').updateOne(
-                { _id: id },
-                { $set: { image: imagePath } }
-            ).then(() => {
-                callback(id);
-            }).catch(err => {
-                console.error("❌ Error updating image path:", err);
-                callback(id);
-            });
-        }).catch((err) => {
-            console.error("❌ Error inserting product:", err);
-            callback(false);
-        });
-    },
+      await dbInstance.collection('products').updateOne(
+        { _id: id },
+        { $set: { image: imagePath } }
+      );
+      callback(id);
+    } catch (err) {
+      console.error("❌ Error in addProduct:", err);
+      callback(false);
+    }
+  },
 
-    getAllProducts: () => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let products = await db.getDB().collection('products').find().toArray();
-                resolve(products);
-            } catch (err) {
-                reject(err);
+  getAllProducts: async () => {
+    try {
+      const dbInstance = db.getDB();
+      const products = await dbInstance.collection('products').find().toArray();
+      return products;
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  deleteProduct: async (proId) => {
+    try {
+      const dbInstance = db.getDB();
+      const objectId = new ObjectId(proId);
+      const response = await dbInstance.collection('products').deleteOne({ _id: objectId });
+      return response;
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  getProductDetails: async (proId) => {
+    try {
+      const dbInstance = db.getDB();
+      const objectId = new ObjectId(proId);
+      const product = await dbInstance.collection('products').findOne({ _id: objectId });
+      return product;
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  updateProduct: async (proId, proDetails) => {
+    try {
+      const dbInstance = db.getDB();
+      const objectId = new ObjectId(proId);
+      const response = await dbInstance.collection('products').updateOne(
+        { _id: objectId },
+        {
+          $set: {
+            name: proDetails.name,
+            description: proDetails.description,
+            category: proDetails.category,
+            price: proDetails.price
+          }
+        }
+      );
+      return response;
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  updateProductImage: async (proId, imagePath) => {
+    const dbInstance = db.getDB();
+    const objectId = new ObjectId(proId);
+    return dbInstance.collection('products').updateOne(
+      { _id: objectId },
+      { $set: { image: imagePath } }
+    );
+  },
+
+  getAllOrders: async () => {
+    try {
+      const dbInstance = db.getDB();
+      const orders = await dbInstance.collection('orders').aggregate([
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'userId',
+            foreignField: '_id',
+            as: 'user'
+          }
+        },
+        { $unwind: '$user' },
+        { $unwind: '$products' },
+        {
+          $lookup: {
+            from: 'products',
+            localField: 'products.productId',
+            foreignField: '_id',
+            as: 'product'
+          }
+        },
+        { $unwind: '$product' },
+        {
+          $group: {
+            _id: "$_id",
+            user: { $first: "$user" },
+            location: { $first: "$location" },
+            date: { $first: "$date" },
+            status: { $first: "$status" },
+            totalAmount: { $first: "$totalAmount" },
+            products: {
+              $push: {
+                name: "$product.name",
+                price: "$product.price",
+                quantity: "$products.quantity",
+                image: "$product.image"
+              }
             }
-        });
-    },
+          }
+        },
+        { $sort: { date: -1 } }
+      ]).toArray();
 
-    deletePRoduct: (proId) => {
-        return new Promise((resolve, reject) => {
-            const objectId = new ObjectId(proId);
-            db.getDB().collection('products').deleteOne({ _id: objectId })
-                .then((response) => {
-                    console.log("Delete response:", response);
-                    resolve(response);
-                })
-                .catch((err) => {
-                    console.error("Delete error:", err);
-                    reject(err);
-                });
-        });
-    },
+      return orders;
+    } catch (err) {
+      console.error("❌ Error getting all orders:", err);
+      throw err;
+    }
+  },
 
-    getProductDetails: (proId) => {
-        const objectId = new ObjectId(proId);
-        return new Promise((resolve, reject) => {
-            db.getDB().collection('products').findOne({ _id: objectId })
-                .then((product) => {
-                    resolve(product);
-                }).catch(reject);
-        });
-    },
-    updateProduct: (proId, proDetails) => {
-        const objectId = new ObjectId(proId);
-        return new Promise((resolve, reject) => {
-            db.getDB().collection('products').updateOne(
-                { _id: objectId },
-                {
-                    $set: {
-                        name: proDetails.name,
-                        description: proDetails.description, // fixed spelling
-                        category: proDetails.category,
-                        price: proDetails.price
-                    }
-                }
-            ).then((response) => {
-                resolve(response);
-            }).catch((err) => {
-                reject(err);
-            });
-        });
-    },
-    updateProductImage: (proId, imagePath) => {
-        const objectId = new ObjectId(proId);
-        return db.getDB().collection('products').updateOne(
-          { _id: objectId },
-          { $set: { image: imagePath } }
-        );
-      },
-      getAllOrders: async () => {
-        try {
-          const orders = await db.getDB().collection('orders').aggregate([
-            {
-              $lookup: {
-                from: 'users',
-                localField: 'userId',
-                foreignField: '_id',
-                as: 'user'
-              }
-            },
-            { $unwind: '$user' },
-      
-            { $unwind: '$products' },
-      
-            {
-              $lookup: {
-                from: 'products',
-                localField: 'products.productId',
-                foreignField: '_id',
-                as: 'product'
-              }
-            },
-            { $unwind: '$product' },
-      
-            {
-              $group: {
-                _id: "$_id",
-                user: { $first: "$user" },
-                location: { $first: "$location" },
-                date: { $first: "$date" },
-                status: { $first: "$status" },
-                totalAmount: { $first: "$totalAmount" },
-                products: {
-                  $push: {
-                    name: "$product.name",
-                    price: "$product.price",
-                    quantity: "$products.quantity",
-                    image: "$product.image"
-                  }
-                }
-              }
-            },
-            { $sort: { date: -1 } }
-          ]).toArray();
-      
-          return orders;
-        } catch (err) {
-          console.error("❌ Error getting all orders:", err);
-          throw err;
-        }
-      },
-      
-        searchProducts: async (query) => {
-        try {
-          const searchRegex = new RegExp(query, 'i'); // case-insensitive
-          const results = await db.getDB().collection('products').find({
-            $or: [
-              { name: { $regex: searchRegex } },
-              { category: { $regex: searchRegex } }
-            ]
-          }).toArray();
-      
-          return results;
-        } catch (err) {
-          throw err;
-        }
-      }
-      
-      
-      
-}
+  searchProducts: async (query) => {
+    try {
+      const dbInstance = db.getDB();
+      const searchRegex = new RegExp(query, 'i'); // case-insensitive
+      const results = await dbInstance.collection('products').find({
+        $or: [
+          { name: { $regex: searchRegex } },
+          { category: { $regex: searchRegex } }
+        ]
+      }).toArray();
+      return results;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
